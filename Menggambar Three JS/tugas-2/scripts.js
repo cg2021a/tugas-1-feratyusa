ff9379cbbadc7688bafcd04df9ea3af5c30089e5
feratyusa <prabuega55@gmail.com>
2021-10-17 13:57:28 +0700

    add prototipe

--- Menggambar Three JS/tugas-2/scripts.js
+++ Menggambar Three JS/tugas-2/scripts.js
@@ -1,19 +1,34 @@
 // Variables
+let boxes = [];
+let score = 0;
+let selected0 = null;
+let selected1 = null;
+const MAX_CUBE = 30;
+const customSizes = {
+    width: window.innerWidth * 0.9,
+    height: window.innerHeight * 0.9
+};
 
+let total_cube = 0;
 
 // canvas
 const canvas = document.getElementById("myCanvas");
 
 // scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color('grey');
+scene.background = new THREE.Color('black');
 
 // camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(0, 0, 10);
+const camera = new THREE.PerspectiveCamera(75, customSizes.width / customSizes.height, 1, 1000);
+camera.position.set(0, 0, 40);
 
-// Initiate Distinct Color
-const distinctColors = ['red', 'blue', 'green', 'purple', 'black'];
+// raycast mouse
+const rayCast = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+mouse.x = mouse.y = -1;
+
+// Define Distinct Color
+const distinctColors = ['red', 'blue', 'green', 'purple'];
 const minColor = 0;
 const maxColor = distinctColors.length;
 
@@ -28,37 +43,114 @@ const pickColor = function(){
     return distinctColors[randNumber];
 }
 
+// Spawn Plane
+const spawnPlane = function(){
+    let geometry = new THREE.PlaneGeometry(80, 80);
+    let material = new THREE.MeshBasicMaterial({color: new THREE.Color('white')});
+    let plane = new THREE.Mesh(geometry, material);
+    plane.position.set(0, -30, 0);
+    plane.rotation.x = Math.PI * -0.5;
+    scene.add(plane);
+}
+
 // Spawn Cube
 const spawnCube = function(randColor){
-    let geometry = new THREE.BoxGeometry(1,1,1);
+    let geometry = new THREE.BoxGeometry(3, 3, 3);
     let material = new THREE.MeshBasicMaterial({color: new THREE.Color(randColor)});
     let box = new THREE.Mesh(geometry, material);
-    box.position.x = pickNumber(-10, 10);
-    box.position.y = pickNumber(-10, 10);
-    box.position.z = pickNumber(-10, 10);
+    box.position.x = pickNumber(-20, 20);
+    box.position.y = pickNumber(-20, 20);
+    box.position.z = pickNumber(-20, 20);
+    // boxes.push(box);
     scene.add(box);
 }
 
-// Testing
-spawnCube(pickColor());
+// Chcek if selected is same color
+const checkSelected = function(){
+    if(selected0.material.color.equals(selected1.material.color) && selected0 != selected1){
+        // Remove the two object
+        scene.remove(selected0.object); scene.remove(selected1);
+        selected0.geometry.dispose();
+        selected0.material.dispose();
+        selected1.geometry.dispose();
+        selected1.material.dispose();
+
+        selected0 = null;
+        selected0 = null;
+
+        total_cube -= 2;
+        score++;
+        console.log(score);
+    }
+    else{
+        // Revert the object back to its original color
+        // selected0.object.material.color = selected0.color;
+        // selected1.object.material.color = selected1.color;
+
+        selected0 = null;
+        selected1 = null;
+        return;
+    };
+}
+
+// Mouseclik event
+let onMouseClick = function(e) {
+    mouse.x = (e.clientX / customSizes.width) * 2 - 1;
+    mouse.y = - (e.clientY / customSizes.height) * 2 + 1;
+
+    rayCast.setFromCamera(mouse, camera);
+    let intersects = rayCast.intersectObjects(scene.children, false);
+    intersects.forEach(obj =>{
+        if(! (obj.object.material.color.equals(new THREE.Color('white'))) ){
+            if( selected0 === null ){
+                selected0 = obj.object;
+                // obj.object.material.color = new THREE.Color('grey');
+                // console.log(selected0.color)
+            }
+            else if( selected1 === null ){
+                selected1 = obj.object;
+                // console.log(selected1.color)
+            }
+        }
+    });
+};
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({canvas: canvas, alpha: true});
-renderer.setSize(0.9 * window.innerWidth, 0.9 * window.innerHeight);
-// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(customSizes.width, customSizes.height);
 
-// orbit controls
+// Event Listener
+document.addEventListener("click", onMouseClick, false);
+
+// Orbit controls
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.listenToKeyEvents(window);
 controls.enableDampling = true;
+controls.update();
 controls.damplingFactor = 0.5;
-controls.autoRotate = false;
-controls.screenSpacePanning = false;
+
+// Testing
+let spawn_cube = 0;
+let spawn_cd = 100;
+const spawn_rate = 0.5;
+spawnPlane();
 
 // MainLoop
 const mainloop = function(){
+    if(spawn_cube == spawn_cd && total_cube <= MAX_CUBE){
+        const color = pickColor();
+        spawnCube(color);
+        total_cube++;
+        if(spawn_cd > 0)
+            spawn_cd -= 5;
+        spawn_cube = 0;
+    }
+
+    spawn_cube += spawn_rate;
+
     renderer.render(scene, camera);
     controls.update();
+    checkSelected();
     requestAnimationFrame(mainloop);
 }
 
